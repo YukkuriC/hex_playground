dbf00c13d966a434a8e65ab02aa398e970eb34dc
YukkuriC <799433638@qq.com>
2024-10-18 00:27:07 +0800

    隔离作用域

--- startup_scripts/Helpers.js
+++ startup_scripts/Helpers.js
@@ -1,40 +1,42 @@
 // chain block breaking
-const MAX_CHAIN = 4096
-const DELTA_DIRS = []
-for (let d of [-1, 1]) {
-    DELTA_DIRS.push([d, 0, 0], [0, d, 0], [0, 0, d])
-    for (let d2 of [-1, 1]) {
-        DELTA_DIRS.push([d, d2, 0], [0, d, d2], [d2, 0, d])
+{
+    let MAX_CHAIN = 4096
+    let DELTA_DIRS = []
+    for (let d of [-1, 1]) {
+        DELTA_DIRS.push([d, 0, 0], [0, d, 0], [0, 0, d])
+        for (let d2 of [-1, 1]) {
+            DELTA_DIRS.push([d, d2, 0], [0, d, d2], [d2, 0, d])
+        }
     }
-}
 
-/**
- * general helper
- * @param { Internal.Level } level
- * @param { BlockPos } blockPos
- * @param { (block:Internal.BlockContainerJS) => boolean } predicate
- * @param { (block:Internal.BlockContainerJS) => void } callback
- */
-global.FloodFillBlocks = (level, blockPos, predicate, callback) => {
-    let blockQueue = [blockPos]
-    let visited = new Set()
-    visited.add(blockPos.hashCode())
-    for (let _ = 0; _ < MAX_CHAIN && blockQueue.length > 0; ) {
-        let pos = blockQueue.shift()
-        let { x, y, z } = pos
-        let bb = level.getBlock(pos)
-        if (!predicate(bb)) continue
-        callback(bb)
-        _++
+    /**
+     * general helper
+     * @param { Internal.Level } level
+     * @param { BlockPos } blockPos
+     * @param { (block:Internal.BlockContainerJS) => boolean } predicate
+     * @param { (block:Internal.BlockContainerJS) => void } callback
+     */
+    global.FloodFillBlocks = (level, blockPos, predicate, callback) => {
+        let blockQueue = [blockPos]
+        let visited = new Set()
+        visited.add(blockPos.hashCode())
+        for (let _ = 0; _ < MAX_CHAIN && blockQueue.length > 0; ) {
+            let pos = blockQueue.shift()
+            let { x, y, z } = pos
+            let bb = level.getBlock(pos)
+            if (!predicate(bb)) continue
+            callback(bb)
+            _++
 
-        // dfs
-        for (let delta of DELTA_DIRS) {
-            let [i, j, k] = delta
-            let newPos = new BlockPos(x + i, y + j, z + k)
-            let newKey = newPos.hashCode()
-            if (!visited.has(newKey)) {
-                visited.add(newKey)
-                blockQueue.push(newPos)
+            // dfs
+            for (let delta of DELTA_DIRS) {
+                let [i, j, k] = delta
+                let newPos = new BlockPos(x + i, y + j, z + k)
+                let newKey = newPos.hashCode()
+                if (!visited.has(newKey)) {
+                    visited.add(newKey)
+                    blockQueue.push(newPos)
+                }
             }
         }
     }
