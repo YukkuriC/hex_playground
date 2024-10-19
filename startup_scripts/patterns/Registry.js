ff0e99c753c52623582af872df8ba4a8672a079f
YukkuriC <799433638@qq.com>
2024-10-20 00:56:39 +0800

    文件拆分

---
+++ startup_scripts/patterns/Registry.js
@@ -0,0 +1,23 @@
+global.loadCustomPatterns = () => {
+    let actionLookup = global.getField(PatternRegistry, 'actionLookup', 1)
+    function registerPatternWrap(seq, dir, id, isGreat) {
+        isGreat = !!isGreat
+        if (!id in global.PatternOperateMap) throw new Error('missing operate: ' + id)
+        let resourceKey = ResourceLocation('yc', id)
+        if (actionLookup.containsKey(resourceKey)) actionLookup.remove(resourceKey)
+        PatternRegistry.mapPattern(HexPattern.fromAngles(seq, dir), resourceKey, new ActionJS(id, isGreat), isGreat)
+    }
+
+    registerPatternWrap('aaqawawaeadaadadadaadadadaada', HexDir.EAST, 'floodfill', 1)
+    registerPatternWrap('wwaqqqqqedwdwwwaw', HexDir.EAST, 'charge_media', 1)
+    registerPatternWrap('aaddwdwdqdwd', HexDir.NORTH_WEST, 'punch_entity')
+    registerPatternWrap('wqqwqwqaeqeeedqqeaqadedaqaedeqqeqedeqeaqeqaqedeadeaqwqwqaeda', HexDir.EAST, 'brain_merge')
+
+    registerPatternWrap('wewewewewewweeqeeqeeqeeqeeqee', HexDir.WEST, 'refresh_depth', 1)
+    registerPatternWrap('waawweeeeedd', HexDir.SOUTH_WEST, 'mind_stack/push')
+    registerPatternWrap('wqaqwweeeee', HexDir.SOUTH_WEST, 'mind_stack/pop')
+    registerPatternWrap('waawweeeeewaa', HexDir.SOUTH_WEST, 'mind_stack/size')
+    registerPatternWrap('waawweeeeaaeaeaeaeaw', HexDir.SOUTH_WEST, 'mind_patterns')
+    registerPatternWrap('waawweeeeewdewqa', HexDir.SOUTH_WEST, 'mind_patterns/clear')
+}
+StartupEvents.postInit(global.loadCustomPatterns)
---
+++ startup_scripts/patterns/RegistryBase.js
@@ -0,0 +1,57 @@
+// priority:10
+
+function Args(stack, n, keep) {
+    if (stack.length < n) throw MishapNotEnoughArgs(n, stack.length)
+    this.data = stack[keep ? 'slice' : 'splice'](-n)
+}
+let _buildGetter = (key, keyMishap) => {
+    keyMishap = keyMishap || key
+    keyMishap = 'class.' + keyMishap
+    return function (i) {
+        let iota = this.data[i]
+        let res = iota[key]
+        if (res === undefined) throw MishapInvalidIota.of(iota, this.data.length - i - 1, keyMishap)
+        return res
+    }
+}
+Args.prototype = {
+    get(i) {
+        return this.data[i]
+    },
+    brainsweep_target(i) {
+        let entity = this.entity(i)
+        if (entity instanceof Mob) {
+            if (Brainsweeping.isValidTarget(entity)) return entity
+            if (entity instanceof AbstractVillager) return entity
+        }
+        throw MishapInvalidIota.of(this.data[i], this.data.length - i - 1, 'class.entity.brainsweep_target')
+    },
+}
+for (let pair of ['double', 'entity', 'list', 'pattern', 'vec3/vector', 'bool/boolean']) {
+    let [key, keyMishap] = pair.split('/')
+    Args.prototype[key] = _buildGetter(key, keyMishap)
+}
+
+function ActionJS(id, isGreat) {
+    this.operate = (c, s, r, ct) => {
+        s = Array.from(s.toArray()) // for js methods
+        try {
+            return global.PatternOperateMap[id](c, s, r, ct) || OperationResult(c, s, r, [])
+        } catch (e) {
+            if (e instanceof Mishap)
+                return OperationResult(c, s, r, [OperatorSideEffect.DoMishap(e, Mishap.Context(HexPattern(HexDir.WEST, []), null))])
+            throw e
+        }
+    }
+
+    // isGreat
+    this.isGreat = isGreat ? () => true : () => false
+
+    // TODO displayName by lang
+    let _displayName = Text.translate(`hexcasting.spell.yc:${id}`).gold()
+    this.getDisplayName = this.displayName = () => _displayName
+}
+ActionJS.prototype = {
+    alwaysProcessGreatSpell: () => true,
+    causesBlindDiversion: () => true,
+}
