90d1b5666716b6de2e5f947dda573ad8b8b8cfbc
YukkuriC <799433638@qq.com>
2024-10-26 20:21:49 +0800

    parser only

--- startup_scripts/reflection/ClassOp.js
+++
@@ -1,7 +0,0 @@
-// priority:114514
-
-global.toRawClass = cls => {
-    let clsName = String(cls).match(/[\w\.]+(?=\]?$)/)[0]
-    return global.loadRawClass(clsName)
-}
-global.loadRawClass = clsName => Java.class.class.forName(clsName)
