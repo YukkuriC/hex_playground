130eebc6924f7c6ea8cccf3dbe17471307ac2086
YukkuriC <799433638@qq.com>
2025-04-01 00:06:22 +0800

    utils & more abstraction

--- startup_scripts/OJ/AllProblems.js
+++ startup_scripts/OJ/AllProblems.js
@@ -2,3 +2,27 @@
 
 new Problem(0)
 new Problem(1, { caseCount: 3 })
+new Problem(2, {
+    onSubmit(stack) {
+        this.tryCompare(new Args(stack, 1).entity(0), this.env.caster)
+    },
+})
+
+// simple case gen
+Problem.expectNumber(3, {
+    caseCount: 5,
+    genAnswer(stack) {
+        stack.push(DoubleIota(this.caseIdx))
+        return this.caseIdx * 2
+    },
+})
+Problem.expectNumber(4, {
+    caseCount: 5,
+    genAnswer(stack) {
+        let a = 3 * (this.caseIdx + 1)
+        let b = 1 + Math.floor(Math.random() * 100)
+        stack.push(DoubleIota(a))
+        stack.push(DoubleIota(b))
+        return a + b
+    },
+})
