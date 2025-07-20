b398e6addd261532d54f02d96a70d0f80d61bc4a
YukkuriC <799433638@qq.com>
2025-07-21 01:01:27 +0800

    adjust training mechanism

--- server_scripts/HexOP_TrainingPersonalMedia.js
+++ server_scripts/HexOP_TrainingPersonalMedia.js
@@ -1,8 +1,9 @@
 // requires: hexoverpowered
 {
-    let RATIO_REGEN_TO_MAX = 1e-2
-    let RATIO_OVER_REGEN_TO_MAX = 1e-6
-    let RATIO_CAST_TO_REGEN = 1e-2
+    let RATIO_REGEN_TO_MAX = 2e-2
+    let RATIO_OVER_REGEN_TO_MAX = 1e-5
+    let RATIO_CAST_TO_REGEN = 2e-3
+    let RATIO_HURT_TO_MAX = 1000
     let KEY_TRAINING = 'hexop_training'
 
     /**
@@ -26,21 +27,32 @@
         if (map.regen) player.modifyAttribute(HexOPAttributes.PERSONAL_MEDIA_REGEN, KEY_TRAINING, map.regen, 'addition')
     }
 
+    let modifyTraining = (player, max, regen) => {
+        let map = getTrainingMap(player)
+        if (max) map.max = (map.max || 0) + max
+        if (regen) map.regen = (map.regen || 0) + regen
+        refreshPersonalMana(player)
+    }
+
     // reset all events
     PersonalManaEvents.resetAll()
 
     // excess regen adds max
     PersonalManaEvents.AddOnInsert(e => {
-        let map = getTrainingMap(e.player)
-        map.max = (map.max || 0) + e.actual * RATIO_REGEN_TO_MAX + e.dropped * RATIO_OVER_REGEN_TO_MAX
-        refreshPersonalMana(e.player)
+        modifyTraining(e.player, e.actual * RATIO_REGEN_TO_MAX + e.dropped * RATIO_OVER_REGEN_TO_MAX, 0)
     })
 
     // successful usage adds regen
     PersonalManaEvents.AddOnExtract(e => {
-        let map = getTrainingMap(e.player)
-        map.regen = (map.regen || 0) + e.actual * RATIO_CAST_TO_REGEN
-        refreshPersonalMana(e.player)
+        modifyTraining(e.player, 0, e.actual * RATIO_CAST_TO_REGEN)
+    })
+
+    // overcast adds max
+    EntityEvents.hurt(e => {
+        let { player, source } = e
+        if (player && source.getType() == 'hexcasting.overcast') {
+            modifyTraining(player, RATIO_HURT_TO_MAX * e.damage, 0)
+        }
     })
 
     // init on load
