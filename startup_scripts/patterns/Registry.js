c3fcf93e588659de507ec4b7d12f6ca3e14274c8
YukkuriC <799433638@qq.com>
2025-11-23 15:11:48 +0800

    再拆一份给hexop

--- startup_scripts/patterns/Registry.js
+++ startup_scripts/patterns/Registry.js
@@ -34,8 +34,8 @@ StartupEvents.registry('hexcasting:action', e => {
     // registerPatternWrap('waawweeeeewaa', HexDir.SOUTH_WEST, 'mind_stack/size')
     // registerPatternWrap('waawweeeeaaeaeaeaeaw', HexDir.SOUTH_WEST, 'mind_patterns')
     registerPatternWrap('waawweeeeewdewqa', HexDir.SOUTH_WEST, 'mind_patterns/clear')
-    registerPatternWrap('waawedaqqqqdeaqq', HexDir.SOUTH_WEST, 'mind_env/schedule')
-    registerPatternWrap('wqaqwweeeeeqdeaqq', HexDir.SOUTH_WEST, 'mind_env/running_code')
+    // registerPatternWrap('waawedaqqqqdeaqq', HexDir.SOUTH_WEST, 'mind_env/schedule')
+    // registerPatternWrap('wqaqwweeeeeqdeaqq', HexDir.SOUTH_WEST, 'mind_env/running_code')
 
     registerPatternWrap('wdwawedqdewawdw', HexDir.SOUTH_WEST, 'nested_modify')
 })
